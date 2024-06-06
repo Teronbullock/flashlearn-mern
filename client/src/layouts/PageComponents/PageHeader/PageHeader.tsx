19b12b85bd945f0c671ee1ec2f17583a5c5dc024
Teron Bullock <teron.bullock@gmail.com>
2024-06-06 10:18:22 -0400

    update page template and content

--- client/src/layouts/PageComponents/PageHeader/PageHeader.tsx
+++ client/src/layouts/PageComponents/PageHeader/PageHeader.tsx
@@ -1,13 +1,21 @@
-const PageHeader = ({ heroClass, ariaLabel, title, subtitle, img }) => {
+import { PageHeaderProps } from "../PageTemplateTypes";
+import './PageHeader.scss';
+import { Link } from "react-router-dom";
+
+const PageHeader = ({ headerData, children }: PageHeaderProps ) => {
+  const { title, copy } = headerData
+
   return (
-    <header className="page-header">
-    <div className="container">
-      <div className="page-header__content">
-        <h1>Page Header</h1>
-        <h2>Subtitle</h2>
+    <header className="page-header bg-white py-4">
+    <div className="container flex justify-between">
+      <div className="page-header__content w-1/2">
+        <h1 className="mb-4">{title}</h1>
+        <h3>{copy}</h3>
       </div>
-      <nav>
-        
+      <nav className="page-header__nav w-2/5 pt-2">
+        <ul className="page-header__nav-list flex justify-between">
+         {children}
+        </ul>
       </nav>
     </div>
   </header>
