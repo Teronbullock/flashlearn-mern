2ed7a64971b99346e19f7312c5ef40fe02b6f69c
Teron Bullock <teron.bullock@gmail.com>
2024-06-18 05:18:11 -0400

    update few files

---
+++ src/routes/Cards/AddCardPage.tsx
@@ -0,0 +1,15 @@
+import PageTemplate from "../../components-layouts/PageComponents/PageTemplate"
+
+const AddCard = () => {
+    return (
+      <PageTemplate 
+        currentPage="addCardPage"
+      >
+      <section className="container py-12">
+        
+      </section>
+    </PageTemplate>
+    )
+}
+
+export default AddCard;
