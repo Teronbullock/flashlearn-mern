f3376a6f8c1a80900dfff03c26c202d467857480
Teron Bullock <teron.bullock@gmail.com>
2024-07-06 03:33:12 -0400

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -7,45 +7,45 @@ import Footer from './layouts/Footer/Footer';
 import PageNotFound from './pages/PageNotFound';
 import Index  from './pages/IndexPage';
 import Register from './pages/RegisterPage';
-import Login from './pages/LoginPage';
-import Dashboard from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 import Profile from './pages/ProfilePage';
 import Set from './pages/SetPage';
 import CardAddEdit from './pages/CardAddEditPage';
-import CreateSet from './pages/SetCreatePage';
-import EditSet from './pages/SetAddEditPage';
 import ViewCards from './pages/ViewCardsPage';
-
+import AddSetPage from './pages/AddSetPage';
+import EditSetPage from './pages/EditSetPage';
 
 const App = () => {
   const {userId, token } = useContext(AuthContext)!;
 
   let routes;
   if (token) {
-    routes = (<>
-    {/* {console.log('Token:', Date() )} */}
-      <Route path='/profile' element={<Profile /> } />
-      <Route path='/dashboard/:userId' element={<Dashboard /> } />
-      <Route path='/set/:setId' element={<Set /> } />
-      <Route path='/set/:setId/card/:cardId/:action' element={<CardAddEdit />} />
-      <Route path='/set/:setId/cards' element={<ViewCards />} />
-      <Route path='/set/:setId/card/:action' element={<CardAddEdit />} />
-      <Route path='/set/:setId/:action' element={<EditSet />} />
-      <Route path='/set/user/:userId/:action' element={<CreateSet />} />
-
-      <Route path="*" element={ <PageNotFound /> } />
-      <Route path='/' element={<Navigate to={`/dashboard/${userId}`} />} />
-    </>);
+    routes = (
+      <>
+        <Route path='/profile' element={<Profile /> } />
+        <Route path='/dashboard/:userId' element={<DashboardPage /> } />
+        <Route path='/set/:setId/card/:cardId/:action' element={<CardAddEdit />} />
+        <Route path='/set/:setId/card/:action' element={<CardAddEdit />} />
+        <Route path='/set/:setId/cards' element={<ViewCards />} />
+        <Route path='/set/:setId/edit' element={<EditSetPage />} />
+        <Route path='/set/user/:userId/add' element={<AddSetPage />} />
+        <Route path='/set/:setId' element={<Set /> } />
+        <Route path='/' element={<Navigate to={`/dashboard/${userId}`} />} />
+        <Route path="*" element={ <PageNotFound /> } />
+      </>
+    );
   } else {
-    routes = (<>
-      <Route path='/' element={<Index />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/login' element={<Login />} />
-      <Route path="*" element={ <PageNotFound /> } />
-    </>);
+    routes = (
+      <>
+        <Route path='/' element={<Index />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path="*" element={ <PageNotFound /> } />
+      </>
+    );
   }
 
-  
   return (
     <>
       <Header />
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { AuthContextProvider } from './context/AuthContext.tsx'
-import { PageTempProvider } from './context/PageTempContext.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <PageTempProvider>
-          <App />
-        </PageTempProvider>
+        <App />
       </AuthContextProvider>
     </BrowserRouter>
-  </StrictMode>,
+  // </StrictMode>,
 )
