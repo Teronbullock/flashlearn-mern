e868908de0588428e5f6b6b80e915747047e70f9
Teron Bullock <teron.bullock@gmail.com>
2024-07-19 01:02:52 -0400

    add view flashcard and flip action

--- src/features/cards/pages/ViewCardsPage.tsx
+++ src/features/cards/pages/ViewCardsPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import PageHero from "../../../layouts/PageComponents/PageHero";
 import PageHeader from "../../../layouts/PageComponents/PageHeader";
@@ -8,23 +7,15 @@ import useGetCardView from "../hooks/useGetCardView";
 
 
 const ViewCardsPage = () => {
-  // const currentPage = 'viewCardsPage';
-  // const { setId } = useParams();
-  // const [searchParams] = useSearchParams();
-  // const page = searchParams.get('page');
-  // const {cards, getCards} = useGetCardView(setId, page);
-  // const [cardArray, setCardArray] = useState(cards);
-
-
-(async ()=>{
-
-  // console.log('viewcard - cards: ',  await getCards());
-})();
-console.log('viewcard - cards: ');
+  const currentPage = 'viewCardsPage';
+  const { setId } = useParams();
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get('page');
+  const { card, cardCount } = useGetCardView(setId, page);
 
   return(
     <main className="main main-viewCardsPage" >
-      {/* <PageHero currentPage={currentPage} className="page-header--index h-[25vh]" />
+      <PageHero currentPage={currentPage} className="page-header--index h-[25vh]" />
       <PageHeader currentPage={currentPage}>
         <ListItemBtn
           className="btn--tertiary btn--large"
@@ -36,9 +27,9 @@ console.log('viewcard - cards: ');
       <ViewCardSection
         page={Number(page)}
         setId={setId}
-        // cards={cards}
-        // {...state.cards}
-      /> */}
+        card={card}
+        cardCount={cardCount}
+      />
     </main>
   );
 }
