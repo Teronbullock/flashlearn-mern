bd3863554781a4eb5ec82e45facbf994dfeade3c
Teron Bullock <teron.bullock@gmail.com>
2026-03-05 18:44:34 -0500

    fix git error

--- packages/schema-db/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { cardsTable, cardsRelations } from './db/cards-schema';
-export { setsTable, setsRelations } from './db/sets-schema';
-export { usersTable, usersRelations } from './db/users-schema';
-export { refreshTokens, refreshTokensRelations } from './db/refresh-tokens-schema';
-export { RegisterSchema, LoginSchema, PasswordSchema, UpdatePasswordSchema, UserInsertSchema, UserSelectSchema, type RegisterType, type LoginType, type PasswordType, type UpdatePasswordType } from './zod/auth-schema';
-export { SetSchema, SetInsertSchema, SetSelectSchema, type SetType, type SetInsertType, type SetSelectType } from './zod/set-schema';
-export { CardsInsertSchema, CardSelectSchema, CardFormSchema, type CardInsertType, type CardSelectType, type CardFormType, type CardType } from './zod/cards-schema';
-//# sourceMappingURL=index.d.ts.map
